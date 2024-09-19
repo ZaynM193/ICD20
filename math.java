cb5fc8baa7e8622b5bd5b1163ee9117c377c8d49
ZaynM193 <1058601@pdsb.net>
2024-09-19 12:27:08 -0400

    Create math.java

---
+++ math.java
@@ -0,0 +1,8 @@
+class math {
+      public static void main(String[] args) {
+        double circle;
+        double radius = 3;
+        circle = 2 * Math.PI * radius;
+        System.out.println("The circumference of a circle of radius " + radius + " is " + circle);
+      }
+}
