65d22280b3088df95656ed5f228ba7e718e83979
ZaynM193 <1058601@pdsb.net>
2024-09-24 14:07:47 -0400

    Create Lesson 3.java

---
+++ Lesson 3.java
@@ -0,0 +1,7 @@
+class Main {
+      public static void main(String[] args) {
+       System.out.println("Welcome to Sanjay's Koala Party!");
+System.out.print("It's possible Sanjay can distribute " + 12);
+System.out.println(" cookies for each bowl.");
+      }
+}
