d34e918e472cc13301ea766bc14d0cf5a3626c53
ZaynM193 <1058601@pdsb.net>
2024-09-24 14:14:52 -0400

    Create Module 3 Lesson.java

---
+++ Module 3 Lesson.java
@@ -0,0 +1,6 @@
+  class Testing {
+      public static void main(String[] args) {
+       System.out.println("Sanjay has "  +  6  +  " bowls for " +  144  + " cookies ");
+       System.out.println("The number of cookies per bowl is:" + 24);
+   }
+  }
