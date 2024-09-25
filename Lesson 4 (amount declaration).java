63b6d78785700c72a7f0eb903dc89b664909b411
ZaynM193 <1058601@pdsb.net>
2024-09-25 12:40:17 -0400

    Create Lesson 4 (amount declaration).java

---
+++ Lesson 4 (amount declaration).java
@@ -0,0 +1,8 @@
+class Main {
+    public static void main(String[] args) {
+      int amount = 24;
+      System.out.println(amount);
+      System.out.println(" Two dozen is:" +  amount);
+        
+    }
+}
